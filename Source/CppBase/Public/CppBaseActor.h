3e36aee49c952fb971e8c442220fd80c277addf2
Natalia Lyukshina <chip5171@gmail.com>
2024-08-23 12:47:53 +0900

    first commit

---
+++ Source/CppBase/Public/CppBaseActor.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CppBaseActor.generated.h"
+
+UCLASS()
+class CPPBASE_API ACppBaseActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACppBaseActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere);
+	FString PlayerName = GetName();
+
+	UPROPERTY(EditInstanceOnly);
+	int EnemyNum = 20;
+
+	UPROPERTY(EditInstanceOnly);
+	bool IsAlive = true;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	void ShowActorInformation();
+
+
+};
